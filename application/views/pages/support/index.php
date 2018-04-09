617de4add1c7b83e395b55c7bcc354b5f9f97738
paShaman <nikitin696@gmail.com>
2018-04-09 13:22:33 +0300

    2018-04-09 start new design

--- application/views/pages/support/index.php
+++ application/views/pages/support/index.php
@@ -2,7 +2,7 @@
 
 <div class="tabs_block tabs_switcher">
     <div class="tabs">
-        <span tab="feedback" class="tab active"><i class="icon-notifications"></i> Обратная связь</span><?if (Access::file('Инструкция_по_работе_с_ЛК_системы_Администратор.docx')){?><span tab="documents" class="tab"><i class="icon-contract"></i> Документы</span><?}?><span tab="polls" class="tab"><i class="icon-analytics"></i> Опросы</span>
+        <span tab="feedback" class="tab active"><i class="icon-notifications"></i> Обратная связь</span><?if (Access::file('Инструкция_по_работе_с_ЛК_системы_Администратор.docx')){?><span tab="documents" class="tab"><i class="icon-contract"></i> Документы</span><?}?>
     </div>
     <div class="tabs_content">
         <div tab_content="feedback" class="tab_content active">
@@ -14,59 +14,5 @@
             <span class="f20">Инструкция по работе с ЛК системы</span> &nbsp; <a href="/file/Инструкция_по_работе_с_ЛК_системы_Администратор.docx" class="btn btn_small"><i class="icon-download icon"></i> Скачать</a>
         </div>
         <?}?>
-        <div tab_content="polls" class="tab_content">
-            <h2>Новый дизайн:</h2>
-
-            По ссылкам находятся рабочие макеты
-
-            <style>
-                .flex{
-                    margin-top: 20px; margin-bottom: 30px;
-                }
-                .flex > div{
-                    margin-right: 50px;
-                }
-                .flex a.fancy{
-                    display: inline-block; width: 300px; margin-top: 20px;
-                }
-                .flex a.fancy img{
-                    max-width: 100%;
-                }
-            </style>
-
-            <div class="flex">
-                <div>
-                    <a href="/new/minimal/index.html" target="_blank" class="f24"><i class="icon-reply"></i> Вариант 1</a><br>
-                    <a href="<?=(Common::getAssetsLink() . 'img/pic/02.jpg')?>" class="fancy" rel="design">
-                        <img src="<?=(Common::getAssetsLink() . 'img/pic/02.jpg')?>">
-                    </a>
-                </div>
-                <div>
-                    <a href="/new/main/index.html" target="_blank" class="f24"><i class="icon-reply"></i> Вариант 2</a><br>
-                    <a href="<?=(Common::getAssetsLink() . 'img/pic/03.jpg')?>" class="fancy" rel="design">
-                        <img src="<?=(Common::getAssetsLink() . 'img/pic/03.jpg')?>">
-                    </a>
-                </div>
-                <div>
-                    <a href="/new/stylish-menu/index.html" target="_blank" class="f24"><i class="icon-reply"></i> Вариант 3</a><br>
-                    <a href="<?=(Common::getAssetsLink() . 'img/pic/04.jpg')?>" class="fancy" rel="design">
-                        <img src="<?=(Common::getAssetsLink() . 'img/pic/04.jpg')?>">
-                    </a>
-                </div>
-            </div>
-
-            <!-- Put this script tag to the <head> of your page -->
-            <script type="text/javascript" src="//vk.com/js/api/openapi.js?152"></script>
-
-            <script type="text/javascript">
-                VK.init({apiId: 6439179, onlyWidgets: true});
-            </script>
-
-            <!-- Put this div tag to the place, where the Poll block will be -->
-            <div id="vk_poll"></div>
-            <script type="text/javascript">
-                VK.Widgets.Poll("vk_poll", {width: "300"}, "290971591_d902f1b5b489160862");
-            </script>
-        </div>
     </div>
 </div>
--- gulpfile.js
+++ gulpfile.js
@@ -51,6 +51,25 @@ gulp.task('image', function () {
         .pipe(gulp.dest(buildPath + 'img'));
 });
 
+gulp.task('adminpro-css', function () {
+    return gulp.src('admin-pro/minimal/css/**/*.css')
+        .pipe(csso())
+        .pipe(gulp.dest('admin-pro/build/minimal/css'))
+});
+
+gulp.task('adminpro-js', function(){
+    return gulp.src('admin-pro/minimal/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('admin-pro/build/minimal/js'))
+});
+
+gulp.task('adminpro-images', function () {
+    return gulp.src('admin-pro/assets/images/**/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('admin-pro/build/assets/images'));
+});
+
+
 gulp.task('watch', ['build'], function() {
     gulp.watch(srcPath + 'js/**/*.js', ['js']);
     gulp.watch(srcPath + 'css/**/*.less', ['css']);
@@ -60,4 +79,5 @@ gulp.task('watch', ['build'], function() {
 gulp.task('build', ['css', 'js', 'fonts', 'image']);
 gulp.task('fast', ['css', 'js']);
 gulp.task('images', ['image']);
+gulp.task('adminpro', ['adminpro-css', 'adminpro-js', 'adminpro-images']);
 gulp.task('default', ['watch']);
