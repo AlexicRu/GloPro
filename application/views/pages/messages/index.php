2e361a201d352c20ef2cca37312b04cfbe26bdd2
paShaman <nikitin696@gmail.com>
2018-10-15 12:08:49 +0300

    2018-10-15 messages

--- application/views/pages/messages/index.php
+++ application/views/pages/messages/index.php
@@ -1,14 +1,22 @@
-<h1>Сообщения</h1>
-
-<div class="block list">
-    <div class="list_header">
-        <div><a href="#" class="mark_read"><i class="fa fa-check"></i> Отметить прочитанным</a></div>
-        <div><div class="fr input_with_icon"><i class="icon-find"></i><form><input type="text" name="m_search" class="input_big input_messages" placeholder="Поиск..." value="<?=$mSearch?>"></form></div></div>
-    </div>
-    <div class="ajax_block_messages_out">
-
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-6">
+                <a href="#" class="mark_read btn-outline-primary <?=Text::BTN?>"><i class="fa fa-check"></i> Отметить прочитанным</a>
+            </div>
+            <div class="col-sm-6">
+                <form class="input-group">
+            <span class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-search"></i></span>
+            </span>
+                    <input type="text" name="m_search" class="form-control input_messages" placeholder="Поиск..." value="<?=$mSearch?>">
+                </form>
+            </div>
+        </div>
     </div>
 </div>
+
+<div class="ajax_block_messages_out"></div>
 
 <script>
     $(function(){
@@ -24,13 +32,32 @@
         });
     });
 
-    function renderAjaxPaginationMessages(data, block) {
-        for(var i = 0 in data){
+    function renderAjaxPaginationMessages(data, block)
+    {
+        /*for(var i = 0 in data){
             var tpl = $('<div class="news_elem"><div class="n_title"></div><div class="n_date gray"></div></div>');
             tpl.find('.n_title').text(data[i]['NOTE_TITLE']);
             tpl.find('.n_date').text(data[i]['NOTE_DATE']);
             tpl.append(data[i]['NOTE_BODY']);
             tpl.addClass('unread' + data[i].STATUS);
+            block.append(tpl);
+        }*/
+
+        for(var i in data){
+            var tpl = $('<li class="media m-b-20 p-20 bg-white">' +
+                '        <div class="media-body">' +
+                '            <h4 class="mt-0 mb-1 n_title" />' +
+                '            <div class="n_date text-muted" />' +
+                '            <div class="n_body m-t-10" />' +
+                '        </div>' +
+                '    </li>');
+
+            if(data[i]['STATUS'] == 0){
+                tpl.addClass('border border-primary');
+            }
+            tpl.find('.n_title').text(data[i]['NOTE_TITLE']).attr('href', '/news/' + data[i].NOTE_ID);
+            tpl.find('.n_date').text(data[i]['NOTE_DATE']);
+            tpl.find('.n_body').html(data[i]['NOTE_BODY']);
             block.append(tpl);
         }
     }
